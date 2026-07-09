c52774c33cbc271c1c9eae13824a545fda2dc655
joiesachdeva <joie.sachdeva@gmail.com>
2026-07-09 08:54:06 +0530

    multihead attention and feed forward

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -82,7 +82,27 @@ class Head(nn.Module):
         out = wei @ v # B,T,C
         return out 
 
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self attention in parallel """
 
+    def __init__(self,num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self,x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity"""
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+    
+    def forward(self,x):
+        return self.net(x)
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
@@ -90,7 +110,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd) 
-        self.sa_head = Head(n_embd)
+        self.sa_head = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -102,6 +123,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
         x = self.sa_head(x)
+        x = self.ffwd(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
